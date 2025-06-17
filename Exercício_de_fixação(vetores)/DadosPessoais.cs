c1aa3b55d68d90def6c871d2ae832b7999c90ebe
lu1zc4rlos <jujuni1000.carlos@gmail.com>
2025-06-17 16:56:12 -0300

    Adicionar arquivos de projeto.

---
+++ Exercício_de_fixação(vetores)/DadosPessoais.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercício_de_fixação_vetores_ {
+    public class DadosPessoais {
+        private string Nome { get; set; }
+        private string Email { get; set; }
+        private int Quarto { get; set; }
+
+        public DadosPessoais(string nome, string email, int quarto) {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+        public override string ToString() {
+            return $"{Quarto}: {Nome}, {Email}";
+        }
+    }
+}
+
+
+
---
+++ Exercício_de_fixação(vetores)/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercício_de_fixação_vetores_ {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Quantos carros seram alugados? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            DadosPessoais[] alugueis = new DadosPessoais[10];
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine($"Rent #{i + 1}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                alugueis[quarto] = new DadosPessoais(nome, email, quarto);
+                Console.Write("\n");
+
+            }
+
+            Console.WriteLine("Busy rooms:");
+
+            for (int i = 0; i < 10; i++) {
+                if (alugueis[i] != null) {
+                    Console.WriteLine(alugueis[i]);
+                }
+            }
+
+        }
+    }
+}
