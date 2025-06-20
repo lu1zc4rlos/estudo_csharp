17b7682b16c9cef781fa796adf4e60e2cd9aef51
lu1zc4rlos <jujuni1000.carlos@gmail.com>
2025-06-20 13:38:53 -0300

    Exercício de fixação(matriz)

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercico_matriz {
+    public class Program {
+        static void Main(string[] args) {
+            Console.Write("Qual sera a ordem da matriz: ");
+            int ordem = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[ordem, ordem];
+
+            for (int i = 0; i < ordem; i++) {
+
+                string[] linha = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < ordem; j++) {
+                    matriz[i, j] = int.Parse(linha[j]);
+                }
+            }
+
+            Console.WriteLine("Diagonal Principal: ");
+            for (int i = 0; i < ordem; i++) {
+                Console.Write($"{matriz[i,i]}\t");
+            }
+
+            int negativos = 0;
+            for (int i = 0; i < ordem; i++) {
+                for (int j = 0; j < ordem; j++) {
+                    if (matriz[i,j] < 0) {
+                        negativos += 1;
+                    }
+                }
+            }
+            Console.WriteLine($"\nNegativos = {negativos}");
+        }
+    }
+}
